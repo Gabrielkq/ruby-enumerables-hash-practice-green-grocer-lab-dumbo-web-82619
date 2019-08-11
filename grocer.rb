930d0415f4e62bb2f49f270915ec420eb0436a22
Gabriel Kutik <gabrielkutik@yahoo.com>
2019-08-11 23:48:04 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -36,7 +36,7 @@ def apply_clearance(cart)
   # code here
   cart.each do |discount|
    if discount[:clearance]  === true
-     discount[:price] = ((discount[price]*.8).)
+     discount[:price] = (discount[price]*.8).round(2)
       
     
     
